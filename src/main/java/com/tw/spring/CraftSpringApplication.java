19ac9a4cbb3d8ef64ef8b92bcaabd1bfd501ef8b
linfeng <1029776125@qq.com>
2024-07-01 23:19:46 +0800

    init project

---
+++ src/main/java/com/tw/spring/CraftSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tw.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftSpringApplication.class, args);
+    }
+
+}
