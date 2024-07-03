f4ec61b3e03dd2a2975ac5d17a5ffc1a58649bd3
linfeng <1029776125@qq.com>
2024-07-03 22:36:49 +0800

    includeFilters

--- src/main/java/com/tw/spring/custom/CustomConfig.java
+++ src/main/java/com/tw/spring/custom/CustomConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 @Configuration
-@ComponentScan(basePackages = "com.tw.spring.custom", excludeFilters = {
-    @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class, Service.class})
-})
+@ComponentScan(basePackages = "com.tw.spring.custom", useDefaultFilters = false,
+    includeFilters = {
+        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Service.class})
+    })
 public class CustomConfig {
 
     @Bean
