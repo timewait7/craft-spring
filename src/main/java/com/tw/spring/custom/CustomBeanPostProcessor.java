fe93948ee9295a9e49a22a23c06beeaff9eb37dd
linfeng <1029776125@qq.com>
2024-07-05 22:41:33 +0800

    test customize BeanPostProcessor

--- src/main/java/com/tw/spring/custom/CustomBeanPostProcessor.java
+++ src/main/java/com/tw/spring/custom/CustomBeanPostProcessor.java
@@ -4,6 +4,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
+/**
+ * BeanPostProcessor先于InitializingBean执行
+ */
 public class CustomBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
